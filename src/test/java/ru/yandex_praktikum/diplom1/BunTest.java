4d44ae4b119040cc38034b8fce7484c7ce584c08
Nikita Pavlutin <schneewald.nick@yandex.ru>
2023-06-27 21:19:29 +0300

    Updates due to reviewer\'s comments

--- src/test/java/ru/yandex_praktikum/diplom1/BunTest.java
+++ src/test/java/ru/yandex_praktikum/diplom1/BunTest.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 @RunWith(MockitoJUnitRunner.class)
 public class BunTest {
+    private static final String EMPTY_STRING = "";
+    private static final String NULL_STRING = null;
     @Mock
     private Database database;
 
@@ -41,6 +43,84 @@ public class BunTest {
     public void getPriceNoParamsShouldReturnPrice(){
         Bun bun = new Bun("white bun", 200);
         Assert.assertEquals(200,bun.getPrice(),0.05);
+    }
+
+    @Test
+    public void createBunWithEmptyStringName(){
+        Bun bun = new Bun("", 200);
+        Assert.assertEquals(EMPTY_STRING,bun.getName());
+    }
+
+    @Test
+    public void createBunWithNullStringName(){
+        Bun bun = new Bun(null,200);
+        Assert.assertEquals(NULL_STRING,bun.getName());
+    }
+
+    @Test
+    public void createBunWithRussianName(){
+        Bun bun = new Bun("Булка с кунжутом", 200);
+        Assert.assertEquals("Булка с кунжутом", bun.getName());
+    }
+
+    @Test
+    public void createBunWithChineseName(){
+        Bun bun = new Bun("芝麻小圆面包",200);
+        Assert.assertEquals("芝麻小圆面包", bun.getName());
+    }
+
+    @Test
+    public void createBunWithNumbersInName(){
+        Bun bun = new Bun("Булка с кунжутом 12345", 200);
+        Assert.assertEquals("Булка с кунжутом 12345", bun.getName());
+    }
+
+    @Test
+    public void createBunWithSpecialCharactersInName(){
+        Bun bun = new Bun("&*^%$^$%$*!",200);
+        Assert.assertEquals("&*^%$^$%$*!", bun.getName());
+    }
+
+    @Test
+    public void createBunWithVeryLongName(){
+        Bun bun = new Bun("Карл у Клары украл кораллы, а Клара у Карла украла кларнет",200);
+        Assert.assertEquals("Карл у Клары украл кораллы, а Клара у Карла украла кларнет",bun.getName());
+    }
+
+    @Test
+    public void createBunWithHyphenatedName(){
+        Bun bun = new Bun("Булочка-с-кунжутом",200);
+        Assert.assertEquals("Булочка-с-кунжутом", bun.getName());
+    }
+
+    @Test
+    public void createBunWithMaximumPrice(){
+        Bun bun = new Bun("Булочка с кунжутом", Float.MAX_VALUE);
+        Assert.assertEquals(Float.MAX_VALUE,bun.getPrice(),0.05);
+    }
+
+    @Test
+    public void createBunWithMinimumPrice(){
+        Bun bun = new Bun("Булочка с кунжутом", Float.MIN_VALUE);
+        Assert.assertEquals(Float.MIN_VALUE,bun.getPrice(),0.05);
+    }
+
+    @Test
+    public void createBunWithZeroPrice(){
+        Bun bun = new Bun("Булочка с кунжутом",0.0f);
+        Assert.assertEquals(0.0f, bun.getPrice(),0.05);
+    }
+
+    @Test
+    public void createBunWithNegativePrice(){
+        Bun bun = new Bun("Булочка с кунжутом", -200f);
+        Assert.assertEquals(-200f, bun.getPrice(),0.05);
+    }
+
+    @Test
+    public void createBunWithMinimalFractionalPartPrice(){
+        Bun bun = new Bun("Булочка с кунжутом", 0.01f);
+        Assert.assertEquals(0.01f, bun.getPrice(),0.005);
     }
 
 }
